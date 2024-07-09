4dd950bb9bdc264f9d9e5ac81eac4c924c37caaf
Marwane El Kchiri <43742934+mxrwn@users.noreply.github.com>
2024-07-09 08:51:17 +0200

    Reformat in monorepo

--- api/apps/api/src/app.module.ts
+++ api/apps/api/src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DevtoolsModule } from '@nestjs/devtools-integration';
 
 @Module({
-  imports: [],
+  imports: [
+    DevtoolsModule.register({
+      http: process.env.NODE_ENV !== 'production',
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
